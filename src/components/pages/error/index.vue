340b54f979ae8ced18d0e08bd03b24a693d9f7eb
zfh123 <1435925296@qq.com>
2017-09-03 12:37:35 +0800

    sdadsa

--- src/components/pages/error/index.vue
+++ src/components/pages/error/index.vue
@@ -11,12 +11,10 @@
         error
         <h1>develop分支发生了变化</h1>
         <h2>master分支的提交</h2>
-<<<<<<< HEAD
         <h3>zfh提交的变更</h3>
-=======
         <h3>63276348</h3>
         <h4>hfdkshfdishfisd</h4>
->>>>>>> develop
+        <h5>dukhdi</h5>
     </div>
 </template>
 <script>
